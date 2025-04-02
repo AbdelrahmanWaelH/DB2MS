97e5187d23c47ec756a59d471873fd8a5760322a
ahmed <ahmedtamer930@gmail.com>
2025-04-02 04:21:26 +0200

    changed structure to make test file work

--- src/main/java/DBMS/Table.java
+++ src/main/java/DBMS/Table.java
@@ -7,4 +7,37 @@ import java.util.Arrays;
 public class Table implements Serializable
 {
 	ArrayList<Page> pages;
+	String[] columns;
+	int columnNumber;
+	int tablePageCount;
+	public Table(String [] columns){
+		pages = null;
+		tablePageCount = 0;
+		this.columns = columns;
+		columnNumber = columns.length;
+	}
+	public void addPage(){
+		this.pages.add(new Page());
+		this.tablePageCount++;
+	}
+
+	public int getLastPageNumOfRecords(){
+		return getLastPage().getNumOfRecords();
+	}
+
+	public Page getLastPage(){
+		if (pages == null){
+			addPage();
+		}
+		return pages.get(tablePageCount - 1);
+	}
+	public boolean canInsertIntoLastPage(int dataPageSize){
+		return getLastPageNumOfRecords() < dataPageSize;
+	}
+	public void insertRecord(String[] row){
+		getLastPage().insertRow(row);
+	}
+	public int getLastPageNumber(){
+		return pages.size()-1;
+	}
 }
